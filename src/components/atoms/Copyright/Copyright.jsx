2a186b52b5be3968dae101da30948545096e5bb0
Serkhan <106021920+southsidescript@users.noreply.github.com>
2025-03-16 15:56:12 -0400

    infoUpdate

--- src/components/atoms/Copyright/Copyright.jsx
+++ src/components/atoms/Copyright/Copyright.jsx
@@ -4,7 +4,7 @@ import style from '@/app/global.module.scss';
 const Copyright = () => {
     return (
         <div className={style.copyright}>
-            <p> &copy; 2025 - rasSVET  - Все права защищены</p>
+            <p> &copy; 2025 - mAlina  - Все права защищены</p>
         </div>
     )
 
